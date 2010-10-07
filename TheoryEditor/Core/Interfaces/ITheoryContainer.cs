6973fa98ddde7b446c65717d29e7869dfc142ff4
Vladihm <protsenkovi@gmail.com>
2010-10-07 22:49:42 +0400

    no changes

---
+++ TheoryEditor/Core/Interfaces/ITheoryContainer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Protsenko.TheoryEditor.Core.Interfaces
+{
+    interface ITheoryContainer
+    {
+        Core.Types.ITheoryElement GetTheoryElement();
+        List<Core.Types.ITheoryElement> GetTheoryElements();
+    }
+}
